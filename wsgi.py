8d6411c91e81ee13756339a9d529f355f6ab6949
shucho28 <shubham.c@practo.com>
2016-06-11 17:05:04 +0530

    openshift migration

---
+++ wsgi.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import os
+
+def application(environ, start_response):
+
+    ctype = 'text/plain'
+    if environ['PATH_INFO'] == '/health':
+        response_body = "1"
+    elif environ['PATH_INFO'] == '/env':
+        response_body = ['%s: %s' % (key, value)
+                    for key, value in sorted(environ.items())]
+        response_body = '\n'.join(response_body)
+    else:
+        ctype = 'text/html'
+        response_body = open(os.environ['OPENSHIFT_REPO_DIR']+'static/index.html','r').read()
+        response_body = response_body.format('static/')
+ 
+    response_body = response_body.encode('utf-8')
+    status = '200 OK'
+    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
+    start_response(status, response_headers)
+    return [response_body ]
+
+#
+# Below for testing only
+#
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    httpd = make_server('localhost', 8051, application)
+    # Wait for a single request, serve it and quit.
+    httpd.handle_request()
